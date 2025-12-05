57b3acc1974ac11696a0025e3f302e57be56c6ae
TheJuanJo1 <mbappejuan464@gmail.com>
2025-12-05 14:22:00 -0500

    Update server.js para que ya acepte a vercel

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ const app = express()
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
+      "https://fluxdata-phi.vercel.app",
       "http://localhost:5173"
     ],
     credentials: true,

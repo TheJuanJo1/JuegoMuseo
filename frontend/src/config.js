4f1ebc3ea5e1326856b12181ba56cd5f6529ad50
Brandon-alt17 <brandonstivenbecerrarangel@gmail.com>
2025-12-09 10:55:30 -0500

    Switch API_URL to use environment variable

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-//export const API_URL = process.env.REACT_APP_BACKEND_URL;
-export const API_URL = "http://localhost:3000"
+export const API_URL = process.env.REACT_APP_BACKEND_URL;
+//export const API_URL = "http://localhost:3000"
 
 if (!API_URL) {
     console.error("ERROR: REACT_APP_BACKEND_URL no está definida")
